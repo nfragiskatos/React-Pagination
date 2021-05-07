920b401b8e6d00bdca876ef69fb402013e2b8a56
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-06 21:20:43 -0700

    Implemented pagination logic
    utils.js
    - implemented paginate function that takes in an array and breaks it up into a 2D array based on the number of items per page.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
-const paginate = () => {}
+const paginate = (followers) => {
+	const itemsPerPage = 9;
+	const numPages = Math.ceil(followers.length / itemsPerPage);
+	const pages = Array.from({ length: numPages }, (_, index) => {
+		const start = index * itemsPerPage;
+		return followers.slice(start, start + itemsPerPage);
+	});
+	return pages;
+};
 
-export default paginate
+export default paginate;
